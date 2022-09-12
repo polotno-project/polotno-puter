b6201e75b5a75ef62f7ed3f193752b541f95c35b
Anton Lavrenov <lavrton@gmail.com>
2022-09-12 13:51:21 -0500

    open image

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createStore } from 'polotno/model/store';
+import { getImageSize } from 'polotno/utils/image';
 
 import './index.css';
 import App from './App';
 
 const store = createStore({ key: 'nFA5H9elEytDyPyvKL7T' });
 window.store = store;
+
+store.addPage();
 
 const url_params = new URLSearchParams(window.location.search);
 function blobToDataURL(blob, callback) {
@@ -26,7 +29,15 @@ if (url_params.has('puter.item.read_url')) {
     .then(async (blob) => {
       // print out the contents of the file
       blobToDataURL(blob, (data_url) => {
-        console.log(data_url);
+        getImageSize(data_url).then((size) => {
+          store.setSize(size.width, size.height);
+          store.activePage.addElement({
+            type: 'image',
+            src: data_url,
+            width: size.width,
+            height: size.height,
+          });
+        });
       });
     });
 }
