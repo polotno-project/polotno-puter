34fa6b3a2a4cd8f66204e31c8ce5cec7dc658ac6
Anton Lavrenov <lavrton@gmail.com>
2022-09-12 13:41:54 -0500

    read file

--- src/index.js
+++ src/index.js
@@ -2,31 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import localforage from 'localforage';
 import { createStore } from 'polotno/model/store';
-import { unstable_setRemoveBackgroundEnabled } from 'polotno/config';
 
 import './index.css';
 import App from './App';
 
-unstable_setRemoveBackgroundEnabled(true);
-
 const store = createStore({ key: 'nFA5H9elEytDyPyvKL7T' });
 window.store = store;
 
-localforage.getItem('polotno-state', function (err, json) {
-  if (json) {
-    store.loadJSON(json);
-  }
-  if (!store.pages.length) {
-    store.addPage();
-  }
-});
+const url_params = new URLSearchParams(window.location.search);
+function blobToDataURL(blob, callback) {
+  var a = new FileReader();
+  a.onload = function (e) {
+    callback(e.target.result);
+  };
+  a.readAsDataURL(blob);
+}
 
-store.on('change', () => {
-  try {
-    const json = store.toJSON();
-    localforage.setItem('polotno-state', json);
-  } catch (e) {}
-});
+if (url_params.has('puter.item.read_url')) {
+  // send GET request to puter.item.read_url to get file content
+  fetch(url_params.get('puter.item.read_url'))
+    .then((response) => {
+      return response.blob();
+    })
+    .then(async (blob) => {
+      // print out the contents of the file
+      blobToDataURL(blob, (data_url) => {
+        console.log(data_url);
+      });
+    });
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
